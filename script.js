106d6a11ed8d5378c5f38858640dd35a2687651d
David Afonso Méndez <afonso27000@gmail.com>
2023-07-07 14:05:15 +0100

    Implementing login logic and balance/movements display

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ const btnTransfer = document.querySelector('.form__btn--transfer');
 const btnLoan = document.querySelector('.form__btn--loan');
 const btnClose = document.querySelector('.form__btn--close');
 const btnSort = document.querySelector('.btn--sort');
+const arrowIcon = document.getElementById('arrow');
 
 const inputLoginUsername = document.querySelector('.login__input--user');
 const inputLoginPin = document.querySelector('.login__input--pin');
@@ -60,3 +61,77 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+let currentAccount = false
+let sortMovements = false
+
+const displayMovements = function(account, sort = false) {
+    containerMovements.innerHTML = ''
+    movements = sort ? account.movements.slice().sort((a, b) => a - b) : account.movements
+    
+    let type;
+    let balance = 0
+    let valueIn = 0
+    let valueOut = 0
+    let valueInterest = movements
+    .filter(movement => movement > 0)
+    .map(deposit => (deposit * account.interestRate) / 100)
+    .filter((int, i, array) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+
+    movements.forEach(function(value, index, movements) {
+      balance += value
+      
+      if(value > 0) {
+        valueIn += value
+        type = 'deposit'
+      } else {
+        valueOut += value
+        type = 'withdrawal'
+      }
+      
+      html = 
+      `
+      <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${index+1} ${type.toUpperCase()}</div>
+      <div class="movements__date">3 days ago</div>
+      <div class="movements__value">${value} €</div>
+      </div>
+      `
+      containerMovements.insertAdjacentHTML('afterbegin', html);
+    })
+    
+    labelBalance.innerHTML = balance + ' €'
+    labelSumIn.innerHTML = valueIn + ' €'
+    labelSumOut.innerHTML = String(valueOut).replace('-', '') + ' €'
+    labelSumInterest.innerHTML = valueInterest + ' €'
+}
+
+btnSort.addEventListener('click', function(e) {
+  e.preventDefault()
+  sortMovements = !sortMovements
+
+  arrowIcon.classList.contains('rotate')
+  ? arrowIcon.classList.remove('rotate')
+  : arrowIcon.classList.add('rotate')
+
+  displayMovements(currentAccount, sortMovements)
+})
+  
+// ~~~~~ Login logic ~~~~~~
+
+btnLogin.addEventListener('click', function(e) {
+  e.preventDefault()
+  accounts.forEach(function(account) {
+    let user = account.owner.split(' ')
+    username = user[0][0] + user[1][0]
+    
+    if(inputLoginUsername.value.toLowerCase().trim() === username.toLowerCase() && Number(inputLoginPin.value.trim()) === account.pin) {
+      currentAccount = account
+      containerApp.style.opacity = 100
+      displayMovements(account)
+    }
+  })
+})
